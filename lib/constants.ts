95233c343d56b7956a5bb2301fb3b7a5504b3e04
jossyboydgenius <jossyboydgenius@gmail.com>
2025-04-29 22:43:03 +0000

    Add Chart Colors: Define color constants for chart components

---
+++ lib/constants.ts
@@ -0,0 +1,9 @@
+// Chart color constants - using exact hex values
+export const CHART_COLORS = {
+  purple: '#AF52DE',
+  orange: '#FF9500',
+  // Add color names that match CSS variable names for easier reference
+  'chart-purple': '#AF52DE',
+  'chart-orange': '#FF9500',
+  // Add more as needed
+};
